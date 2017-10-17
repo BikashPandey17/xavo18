04513bfafa18b4f3be5119ea7b139fe06b48b2b4
Jimut <jimutbahanpal@yahoo.com>
2017-10-17 16:56:26 +0530

    Navbar in progress

---
+++ ver3/sponsors.php
@@ -0,0 +1,52 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Nosifer" rel="stylesheet">
+
+<script src="frontpage.css"></script>
+
+<style>
+
+div.xavo{
+  font-family: 'Nosifer', cursive;
+  font-size: 40px;
+}
+
+
+
+</style>
+
+<!-- Nav Bar               -------------------------------------------------------------------------------------------------------------------------------------------->
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="events.php">EVENTS</a></li>
+      <li ><a href="aboutUS.php">ABOUT US</a></li>
+      <li><a href="schedule.php">SCHEDULE</a></li>
+      <li ><a href="CR.php">COLLEGE REPRESENTATIVE</a></li>
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">
+          <div class="xavo">
+          XAVOTSAV
+        </div>
+        </a>
+      </div>
+      <li class="active"><a href="sponsors.php">SPONSORS</a></li>
+      <li><a href="starNight.php">STAR NIGHT</a></li>
+      <li><a href="gallery.php">GALLERY</a></li>
+      <li><a href="meetTheTeam.php">MEET THE TEAM</a></li>
+      <li><a href="pastLineup.php">PAST LINEUP</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------->

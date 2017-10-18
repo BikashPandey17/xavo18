ea137a38a150c0a8979a7b3a0d361983559e8629
Jimut <jimutbahanpal@yahoo.com>
2017-10-18 11:34:55 +0530

    added deleted

---
+++ ver3/CR.php
@@ -0,0 +1,118 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-------------------Mobile responsive ---------------->
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Nosifer" rel="stylesheet">
+
+<script src="frontpage.css"></script>
+
+<style>
+
+div.xavo{
+  font-family: 'Nosifer', cursive;
+  font-size: 40px;
+}
+div.navbar_xavo{
+  float: center;
+  text-align: center;
+}
+
+
+ul {
+    list-style-type: none;
+    margin: 0px;
+    padding:15px;
+}
+
+section.search_bar_right{
+  align-self: center;
+}
+div.Search{
+  float: center;
+}
+</style>
+
+
+<!-- Nav Bar               -------------------------------------------------------------------------------------------------------------------------------------------->
+<nav class=" navbar navbar-inverse">
+  <section class="navbar_xavo">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="events.php">EVENTS</a></li>
+      <li ><a href="aboutUS.php">ABOUT US</a></li>
+      <li><a href="schedule.php">SCHEDULE</a></li>
+      <li class="active"><a href="CR.php">COLLEGE REPRESENTATIVE</a></li>
+    </ul>
+    <ul class="nav navbar-nav ">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">
+          <div class="xavo">
+          XAVOTSAV
+        </div>
+        </a>
+      </div>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="sponsors.php">SPONSORS</a></li>
+      <li><a href="starNight.php">STAR NIGHT</a></li>
+      <li><a href="gallery.php">GALLERY</a></li>
+      <li><a href="meetTheTeam.php">MEET THE TEAM</a></li>
+      <li><a href="pastLineup.php">PAST LINEUP</a></li>
+
+      <li>
+      <div class="Search search_bar_right">
+        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+          Search
+          <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+          <li>
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search">
+              <div class="input-group-btn">
+                <section class ="search_bar">
+                <button class="btn btn-default" type="submit">
+                  <i class="glyphicon glyphicon-search"></i>
+                </button>
+              </section>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </li>
+<!---
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Search">
+      <div class="input-group-btn">
+        <section class ="search_bar">
+        <button class="btn btn-default" type="submit">
+          <i class="glyphicon glyphicon-search"></i>
+        </button>
+      </section>
+      </div>
+    </div>
+
+-->
+        </ul>
+  </div>
+  </div>
+</section>
+</nav>
+
+
+<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+<footer>
+
+
+
+</footer>

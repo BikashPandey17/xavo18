835393f2f230efec4dfe6bce71bbd0b25ee7537c
Bikash Pandey <pandeybikash98@gmail.com>
2017-10-18 22:43:49 +0530

    college rep panel see extended description.
    Sections "upload" and "p1" are the only ones live. Dashboard section will contain all the tiles (for events). I'll be adding the basic functionalities for the different sections meanwhile you guys look into the front-end code add something if needed. PHP code is not our concern I added those just to link up with the local database to see the front-end code working (it will be changed later so don't bother), link it up with your own local database to see it working.

---
+++ college-rep/p1.php
@@ -0,0 +1,225 @@
+<?php  
+//p1.php
+$connect = mysqli_connect("localhost", "root", "", "testing");
+$query = "SELECT * FROM tbl_employee ORDER BY id DESC";
+$result = mysqli_query($connect, $query);
+ ?>  
+<!DOCTYPE html>  
+<html>  
+ <head>  
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>P1-Dark Theme</title> 
+     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/local.css" />
+
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
+<!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+ </head>  
+ <body>  
+       <div id="wrapper">
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Back to Admin</a>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li><a href="index.html"><i class="fa fa-bullseye"></i> Dashboard</a></li>
+                    <li><a href="#"><i class="fa fa-tasks"></i> Portfolio</a></li>
+                    <li><a href="#"><i class="fa fa-globe"></i> Blog</a></li>
+                    <li><a href="#"><i class="fa fa-list-ol"></i> SignUp</a></li>
+                    <li class="selected"><a href="p1.php"><i class="fa fa-font"></i> P1</a></li>
+                    <li><a href="upload.php"><i class="fa fa-font"></i> Upload</a></li>
+                    <li><a href="#"><i class="fa fa-list-ol"></i> Forms</a></li>
+                    <li><a href="#"><i class="fa fa-font"></i> Typography</a></li>
+                    <li><a href="#"><i class="fa fa-list-ul"></i> Bootstrap Elements</a></li>
+                    <li><a href="#"><i class="fa fa-table"></i > Bootstrap Grid</a></li>            
+                </ul>
+                <ul class="nav navbar-nav navbar-right navbar-user">
+                    <li class="dropdown messages-dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> Messages <span class="badge">2</span> <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-header">2 New Messages</li>
+                            <li class="message-preview">
+                                <a href="#">
+                                    <span class="avatar"><i class="fa fa-bell"></i></span>
+                                    <span class="message">Security alert</span>
+                                </a>
+                            </li>
+                            <li class="divider"></li>
+                            <li class="message-preview">
+                                <a href="#">
+                                    <span class="avatar"><i class="fa fa-bell"></i></span>
+                                    <span class="message">Security alert</span>
+                                </a>
+                            </li>
+                            <li class="divider"></li>
+                            <li><a href="#">Go to Inbox <span class="badge">2</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown user-dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Steve Miller<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+                            <li><a href="#"><i class="fa fa-gear"></i> Settings</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#"><i class="fa fa-power-off"></i> Log Out</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+  <br /><br />  
+  <div class="container" style="width:700px;">  
+   <h3 align="center">Insert Data Through Bootstrap Modal by using Ajax PHP</h3>  
+   <br />  
+   <div class="table-responsive">
+    <div align="right">
+     <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn btn-warning">Add</button>
+    </div>
+    <br />
+    <div id="employee_table">
+     <table class="table table-bordered">
+      <tr>
+       <th width="70%">Employee Name</th>  
+       <th width="30%">View</th>
+      </tr>
+      <?php
+      while($row = mysqli_fetch_array($result))
+      {
+      ?>
+      <tr>
+       <td><?php echo $row["name"]; ?></td>
+       <td><input type="button" name="view" value="view" id="<?php echo $row["id"]; ?>" class="btn btn-info btn-xs view_data" /></td>
+      </tr>
+      <?php
+      }
+      ?>
+     </table>
+    </div>
+   </div>  
+  </div>
+ 
+
+<div id="add_data_Modal" class="modal fade">
+ <div class="modal-dialog">
+  <div class="modal-content">
+   <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal">&times;</button>
+    <h4 class="modal-title">PHP Ajax Insert Data in MySQL By Using Bootstrap Modal</h4>
+   </div>
+   <div class="modal-body">
+    <form method="post" id="insert_form">
+     <label>Enter Employee Name</label>
+     <input type="text" name="name" id="name" class="form-control" />
+     <br />
+     <label>Enter Employee Address</label>
+     <textarea name="address" id="address" class="form-control"></textarea>
+     <br />
+     <label>Select Gender</label>
+     <select name="gender" id="gender" class="form-control">
+      <option value="Male">Male</option>  
+      <option value="Female">Female</option>
+     </select>
+     <br />  
+     <label>Enter Designation</label>
+     <input type="text" name="designation" id="designation" class="form-control" />
+     <br />  
+     <label>Enter Age</label>
+     <input type="text" name="age" id="age" class="form-control" />
+     <br />
+     <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-success" />
+
+    </form>
+   </div>
+   <div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+   </div>
+  </div>
+ </div>
+</div>
+           
+ 
+<div id="dataModal" class="modal fade">
+ <div class="modal-dialog">
+  <div class="modal-content">
+   <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal">&times;</button>
+    <h4 class="modal-title">Employee Details</h4>
+   </div>
+   <div class="modal-body" id="employee_detail">
+    
+   </div>
+   <div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+   </div>
+  </div>
+ </div>
+</div>
+     </div>
+<script>  
+$(document).ready(function(){
+ $('#insert_form').on("submit", function(event){  
+  event.preventDefault();  
+  if($('#name').val() == "")  
+  {  
+   alert("Name is required");  
+  }  
+  else if($('#address').val() == '')  
+  {  
+   alert("Address is required");  
+  }  
+  else if($('#designation').val() == '')
+  {  
+   alert("Designation is required");  
+  }
+   
+  else  
+  {  
+   $.ajax({  
+    url:"insert.php",  
+    method:"POST",  
+    data:$('#insert_form').serialize(),  
+    beforeSend:function(){  
+     $('#insert').val("Inserting");  
+    },  
+    success:function(data){  
+     $('#insert_form')[0].reset();  
+     $('#add_data_Modal').modal('hide');  
+     $('#employee_table').html(data);  
+    }  
+   });  
+  }  
+ });
+
+
+
+
+ $(document).on('click', '.view_data', function(){
+  //$('#dataModal').modal();
+  var employee_id = $(this).attr("id");
+  $.ajax({
+   url:"select.php",
+   method:"POST",
+   data:{employee_id:employee_id},
+   success:function(data){
+    $('#employee_detail').html(data);
+    $('#dataModal').modal('show');
+   }
+  });
+ });
+});  
+ </script>
+  </body>  
+</html>    

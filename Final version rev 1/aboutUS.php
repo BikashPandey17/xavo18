6a1e77273ea8f481bbbef29167ff39403801772c
SouptikMajumdar <32878956+SouptikMajumdar@users.noreply.github.com>
2017-10-20 16:57:57 +0530

    Animations and tiles on home page

---
+++ Final version rev 1/aboutUS.php
@@ -0,0 +1,188 @@
+<html>
+<head>
+
+<title>
+About Us Page of Xavotsav 2018
+</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-------------------Mobile responsive ---------------->
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Nosifer" rel="stylesheet">
+
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="frontpage.css" >
+
+
+</head>
+
+
+
+
+
+
+
+
+
+
+<style>
+
+</style>
+
+
+
+
+<header>
+
+<!-- Nav Bar               -------------------------------------------------------------------------------------------------------------------------------------------->
+<div class="w3-top">
+  <div class="w3-bar" id="myNavbar">
+    <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+      <i class="fa fa-bars"></i>
+      <section class="home">
+    </a>
+    <a href="index.php" class="w3-bar-item w3-button">XAVOTSAV</a>
+    <a href="index.php" class="w3-bar-item w3-button w3-hide-small"> HOME</a>
+    <a href="events.php" class="w3-bar-item w3-button w3-hide-small"> EVENTS</a>
+    <a href="aboutUS.php" class="w3-bar-item w3-button w3-hide-small"> ABOUT US</a>
+    <a href="schedule.php" class="w3-bar-item w3-button w3-hide-small"> SCHEDULE</a>
+    <a href="CR.php" class="w3-bar-item w3-button w3-hide-small"> COLLEGE REPRESENTATIVE</a>
+    <a href="sponsors.php" class="w3-bar-item w3-button w3-hide-small"> SPONSORS</a>
+    <a href="starNight.php" class="w3-bar-item w3-button w3-hide-small"> STAR NIGHT</a>
+    <a href="gallery.php" class="w3-bar-item w3-button w3-hide-small"> GALLERY</a>
+    <a href="meetTheTeam.php" class="w3-bar-item w3-button w3-hide-small"> MEET THE TEAM</a>
+    <a href="pastLineup.php" class="w3-bar-item w3-button w3-hide-small"> PAST LINEUP</a>
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
+    <a href="index.php" class="w3-bar-item w3-button" onclick="toggleFunction()">HOME</a>
+    <a href="events.php" class="w3-bar-item w3-button" onclick="toggleFunction()">EVENTS</a>
+    <a href="aboutUS.php" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT US</a>
+    <a href="schedule.php" class="w3-bar-item w3-button" onclick="toggleFunction()">SCHEDULE</a>
+    <a href="CR.php" class="w3-bar-item w3-button" onclick="toggleFunction()">COLLEGE REPRESENTATIVE</a>
+    <a href="sponsors.php" class="w3-bar-item w3-button" onclick="toggleFunction()">SPONSORS</a>
+    <a href="starNight.php" class="w3-bar-item w3-button" onclick="toggleFunction()">STAR NIGHT</a>
+    <a href="gallery.php" class="w3-bar-item w3-button" onclick="toggleFunction()">GALLERY</a>
+    <a href="meetTheTeam.php" class="w3-bar-item w3-button" onclick="toggleFunction()">MEET THE TEAM</a>
+    <a href="pastLineup.php" class="w3-bar-item w3-button" onclick="toggleFunction()">PAST LINEUP</a>
+    <a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+    </section>
+  </div>
+</div>
+
+<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+</header>
+
+
+
+
+
+
+
+
+<body>
+  LAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+  BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BL
+
+
+</body>
+
+
+
+
+
+
+
+
+
+<footer>
+
+
+
+</footer>
+
+
+
+
+</html>
+
+
+
+
+<script>
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>

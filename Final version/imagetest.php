0712c47f0cb2ce17fef565c5aafc39ee9da76aa7
Jimut <jimutbahanpal@yahoo.com>
2017-10-20 11:40:29 +0530

    FV

---
+++ Final version/imagetest.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+.myLink {display: none}
+</style>
+<body class="w3-light-grey">
+
+
+
+<!-- Page content -->
+<div class="w3-content" style="max-width:1100px;">
+
+  <!-- Good offers -->
+  <div class="w3-container w3-margin-top">
+    <h3>Good Offers Right Now</h3>
+    <h6>Up to <strong>50%</strong> discount.</h6>
+  </div>
+  <div class="w3-row-padding w3-text-white w3-large">
+    <div class="w3-half w3-margin-bottom">
+      <div class="w3-display-container">
+        <img src="../w3images/cinqueterre.jpg" alt="Cinque Terre" style="width:100%">
+        <span class="w3-display-bottomleft w3-padding">Cinque Terre</span>
+      </div>
+    </div>
+    <div class="w3-half">
+      <div class="w3-row-padding" style="margin:0 -16px">
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+            <img src="../w3images/newyork2.jpg" alt="New York" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">New York</span>
+          </div>
+        </div>
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+            <img src="../w3images/sanfran.jpg" alt="San Francisco" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">San Francisco</span>
+          </div>
+        </div>
+      </div>
+      <div class="w3-row-padding" style="margin:0 -16px">
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+            <img src="../w3images/pisa.jpg" alt="Pisa" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">Pisa</span>
+          </div>
+        </div>
+        <div class="w3-half w3-margin-bottom">
+          <div class="w3-display-container">
+            <img src="../w3images/paris.jpg" alt="Paris" style="width:100%">
+            <span class="w3-display-bottomleft w3-padding">Paris</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+</body>
+</html>
---
+++ Final version/index.php
@@ -0,0 +1,412 @@
+
+<html>
+<head>
+
+<title>
+index of Page of Xavotsav 2018
+</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-------------------Mobile responsive ---------------->
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Nosifer" rel="stylesheet">
+
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="frontpage.css" >
+
+
+</head>
+
+
+
+
+
+
+
+
+
+<style>
+
+.abcd {
+-webkit-clip-path: polygon(0 0, 100% 0, 100% 96%, 0 100%);
+clip-path: polygon(10% 0, 100% 0%, 90% 100%, 0 100%);
+}
+  .efgh {
+-webkit-clip-path: polygon(0 0, 100% 0, 100% 96%, 0 100%);
+clip-path: polygon(10% 0, 100% 0%, 90% 100%, 0 100%);
+}
+</style>
+
+
+
+
+
+
+<header>
+
+
+<!-- Nav Bar               -------------------------------------------------------------------------------------------------------------------------------------------->
+<div class="w3-top">
+  <div class="w3-bar" id="myNavbar">
+    <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+      <i class="fa fa-bars"></i>
+    </a>
+    <a href="index.php" class="w3-bar-item w3-button">HOME</a>
+    <a href="events.php" class="w3-bar-item w3-button w3-hide-small"> EVENTS</a>
+    <a href="aboutUS.php" class="w3-bar-item w3-button w3-hide-small"> ABOUT US</a>
+    <a href="schedule.php" class="w3-bar-item w3-button w3-hide-small"> SCHEDULE</a>
+    <a href="CR.php" class="w3-bar-item w3-button w3-hide-small"> COLLEGE REPRESENTATIVE</a>
+    <a href="sponsors.php" class="w3-bar-item w3-button w3-hide-small"> SPONSORS</a>
+    <a href="starNight.php" class="w3-bar-item w3-button w3-hide-small"> STAR NIGHT</a>
+    <a href="gallery.php" class="w3-bar-item w3-button w3-hide-small"> GALLERY</a>
+    <a href="meetTheTeam.php" class="w3-bar-item w3-button w3-hide-small"> MEET THE TEAM</a>
+    <a href="pastLineup.php" class="w3-bar-item w3-button w3-hide-small"> PAST LINEUP</a>
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
+    <a href="events.php" class="w3-bar-item w3-button" onclick="toggleFunction()">EVENTS</a>
+    <a href="aboutUS.php" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT US</a>
+    <a href="schedule.php" class="w3-bar-item w3-button" onclick="toggleFunction()">SCHEDULE</a>
+    <a href="CR.php" class="w3-bar-item w3-button" onclick="toggleFunction()">COLLEGE REPRESENTATIVE</a>
+    <a href="sponsors.php" class="w3-bar-item w3-button" onclick="toggleFunction()">SPONSORS</a>
+    <a href="starNight.php" class="w3-bar-item w3-button" onclick="toggleFunction()">STAR NIGHT</a>
+    <a href="gallery.php" class="w3-bar-item w3-button" onclick="toggleFunction()">GALLERY</a>
+    <a href="meetTheTeam.php" class="w3-bar-item w3-button" onclick="toggleFunction()">MEET THE TEAM</a>
+    <a href="pastLineup.php" class="w3-bar-item w3-button" onclick="toggleFunction()">PAST LINEUP</a>
+    <a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+
+  </div>
+</div>
+<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+</header>
+
+
+
+
+
+
+
+<body>
+<div class="jumbotron">
+
+
+
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAH BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+    BLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAHBLAH BLAH
+
+    <br><br><br><br><br>
+
+
+    <div class="row">
+      <div class="col-md-4">
+
+
+    <div class ="row">
+      <div class="col-md-6">
+
+              <img src="../Xavo ver 4/paris.jpg" alt="Paris" style="width:100%" class="abcd smaller-image">
+            </div>
+
+            <div class="col-md-6">
+              <img src="../Xavo ver 4/paris.jpg" alt="Paris" style="width:100%" class="larger-image efgh">
+            </div>
+
+
+
+      </div>
+
+    </div>
+
+      <div class="col-md-4">
+      </div>
+
+
+
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col-md-6">
+
+            <div class="w3-display-container">
+                <img src="../Xavo ver 4/pisa.jpg" alt="Pisa" style="width:100%">
+                <span class="w3-display-bottomleft w3-padding"><section class="right_images" >Pisa</section></span>
+              </div>
+
+          </div>
+          <div class="col-md-6">
+            <div class="w3-display-container">
+                <img src="../Xavo ver 4/sanfran.jpg" alt="Sanfran" style="width:100%">
+                <span class="w3-display-bottomleft w3-padding"><section class="right_images" >Sanfran</section></span>
+              </div>
+
+          </div>
+
+
+        </div>
+
+    <br>
+        <div class="row">
+          <div class="col-md-6">
+
+            <div class="w3-display-container">
+                <img src="../Xavo ver 4/newyork2.jpg" alt="New York" style="width:100%">
+                <span class="w3-display-bottomleft w3-padding"><section class="right_images" >New York</section></span>
+              </div>
+
+          </div>
+          <div class="col-md-6">
+            <div class="w3-display-container">
+                <img src="../Xavo ver 4/paris.jpg" alt="Paris" style="width:100%">
+                <span class="w3-display-bottomleft w3-padding" ><section class="right_images" >Sanfran</section></span>
+              </div>
+
+          </div>
+
+
+        </div>
+        <br>
+
+        <div class="row">
+          <div class="col-md-6">
+
+            <div class="w3-display-container">
+                <img src="../Xavo ver 4/A.JPG" alt="New York" style="width:100% " >
+                <span class="w3-display-bottomleft w3-padding"><section class="right_images" >Car 1</section></span>
+              </div>
+
+          </div>
+          <div class="col-md-6">
+            <div class="w3-display-container">
+                <img src="../Xavo ver 4/B.jpg" alt="Paris" style="width:100%">
+                <span class="w3-display-bottomleft w3-padding" ><section class="right_images" >Car 2</section></span>
+              </div>
+
+          </div>
+
+
+        </div>
+
+
+
+      </div>
+    </div>
+
+
+
+
+</div>
+
+    </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Footer -->
+    <footer class="w3-container w3-padding-32 w3-dark-grey">
+    <div class="w3-row-padding">
+      <div class="w3-third">
+        <h5>Contact</h5>
+        <p>e-mail : gsec@sxccal.edu </p>
+        <p>phone : +91 90386 34869</p>
+        <h6>For events :</h6>
+        <p>e-mail : pr.xavo18@gmail.com</p>
+        <h6>For PR :</h6>
+        <p>e-mail : eventsxavo18@gmail.com</p>
+      </div>
+
+      <div class="w3-third">
+        <h5>BLOG POSTS</h5>
+        <ul class="w3-ul w3-hoverable">
+          <li class="w3-padding-16">
+            <img src="/w3images/workshop.jpg" class="w3-left w3-margin-right" style="width:50px">
+            <span class="w3-large">Lorem</span><br>
+            <span>Sed mattis nunc</span>
+          </li>
+          <li class="w3-padding-16">
+            <img src="/w3images/gondol.jpg" class="w3-left w3-margin-right" style="width:50px">
+            <span class="w3-large">Ipsum</span><br>
+            <span>Praes tinci sed</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="w3-third">
+        <h5>Social </h5>
+        <p>
+          <div class="w3-xlarge w3-section">
+            <a href="https://facebook.com/xavotsav" alt="facebook link" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+            <a href="https://www.youtube.com/channel/UCUIc-Qc9Jnsh1oE8ZEITsHA" alt="youtube channel" target="_blank"><i class="fa fa-youtube-play w3-hover-opacity"></i></a>
+            <a href="https://www.instagram.com/sxcsc/" alt="instagram link" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+            <a href="https://twitter.com/gensec2016" alt="twitter link" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+              <!--<i class="fa fa-snapchat w3-hover-opacity"></i>
+            <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+
+            <i class="fa fa-linkedin w3-hover-opacity"></i> -->
+
+          </div>
+          <div>
+            <p>
+              Address :
+            </p>
+            <p>
+              30, MOTHER TERESA SARANI, KOLKATA-700016
+            </p>
+            <div>
+        </p>
+      </div>
+
+    </div>
+    </footer>
+
+
+
+  <!-- End page content -->
+
+
+
+  </footer>
+<section class="Jimut">
+<section class="JBP">&copy SXCSC 2017 </section>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+<script>
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
